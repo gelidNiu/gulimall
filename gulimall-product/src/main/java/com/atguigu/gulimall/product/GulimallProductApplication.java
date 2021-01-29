88bef6111d3eb2c38d2955c3d15d898de9dfa8f5
niu <niuxiaotong111@163.com>
2021-01-29 16:54:10 +0800

    加入oss之前

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -1,13 +1,34 @@
 package com.atguigu.gulimall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
+@MapperScan("com/atguigu/gulimall/product/dao")
 @SpringBootApplication
 public class GulimallProductApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(GulimallProductApplication.class, args);
-    }
+//	http://localhost:10001/product/category/list/tree
+//	http://127.0.0.1:88/api/product/category/list/tree
+//	http://127.0.0.1:88/api/product/attrgroup/list/1?page=1&key=aa
 
+//	JSR303最终测试：POSTman :{"name":"aaa","logo":"https://github.com/1046762075","sort":0,"firstLetter":"d","showStatus":0}
+
+//	http://localhost:10001/index/catalog.json
+
+//	分布式锁：http://localhost:10001/index/hello
+
+//	读写锁：http://localhost:10001/index/write
+//			http://localhost:10001/index/read
+
+//	闭锁：	http://localhost:10001/index/lockDoor
+//			http://localhost:10001/index/go/1
+
+//	信号量:	http://localhost:10001/index/go/park
+//			http://localhost:10001/index/park
+	public static void main(String[] args) {
+		SpringApplication.run(GulimallProductApplication.class, args);
+	}
 }
--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,58 @@
 package com.atguigu.gulimall.product;
 
+
+import com.atguigu.gulimall.product.entity.BrandEntity;
+import com.atguigu.gulimall.product.service.BrandService;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
 
+    @Autowired
+    BrandService brandService;
+//
+//    @Autowired
+//    private OSSClient ossClient;
+
+    @Test
+    void testUpload(){
+//        // 创建PutObjectRequest对象。
+//        PutObjectRequest putObjectRequest = new PutObjectRequest("gulimall-niuxiaotong", "华为3", new File("/Users/niuxiaotong/Desktop/谷粒/谷粒商城-github/pic/华为3.jpg"));
+//
+//        // 如果需要上传时设置存储类型与访问权限，请参考以下示例代码。
+//        // ObjectMetadata metadata = new ObjectMetadata();
+//        // metadata.setHeader(OSSHeaders.OSS_STORAGE_CLASS, StorageClass.Standard.toString());
+//        // metadata.setObjectAcl(CannedAccessControlList.Private);
+//        // putObjectRequest.setMetadata(metadata);
+//
+//        // 上传文件。
+//        ossClient.putObject(putObjectRequest);
+//
+//        // 关闭OSSClient。
+//        ossClient.shutdown();
+    }
+
     @Test
     void contextLoads() {
+//        BrandEntity brandEntity=new BrandEntity();
+////        brandEntity.setName("华为");
+////        brandService.save(brandEntity);
+////        System.out.println("保存成功");
+//        brandEntity.setBrandId(1L);
+//        brandEntity.setDescript("华为哈哈哈");
+//        brandService.updateById(brandEntity);
+
+        List<BrandEntity> brand_id = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1L));
+        brand_id.forEach((item)->{
+            System.out.println(item);
+        });
+
+
     }
 
 }
